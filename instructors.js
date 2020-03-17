f938ca63dca9d4aec5a06cdc64c22f84e091a5f7
vtr <vrigolon@gmail.com>
2020-03-17 12:28:50 +0000

    add listing instructors

--- instructors.js
+++ instructors.js
@@ -5,6 +5,12 @@ const data = require( './data.json')
 const { age, date } = require('./utils')
 
 
+exports.index = function(req, res) {
+  return res.render("instructors/index", { instructors: data.instructors })
+}
+
+
+
 //show
 exports.show = function(req, res) {
   const { id } = req.params
@@ -95,8 +101,10 @@ exports.put = function(req, res) {
   const instructor = {
     ...foundInstructor,
     ...req.body,
-    birth: Date.parse(req.body.birth)
+    birth: Date.parse(req.body.birth),
+    id: Number(req.body.id)
   }
+
 
   data.instructors[index] = instructor
 
