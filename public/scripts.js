2af6a0a1cb84d9e3f6498e763ae1972f532fd1ee
vtr <vrigolon@gmail.com>
2020-03-19 13:38:12 +0000

    removed old comments

--- public/scripts.js
+++ public/scripts.js
@@ -7,29 +7,3 @@ for(item of menuItems) {
     item.classList.add("active")
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-// const modalOverlay = document.querySelector('.modal-overlay')
-
-// const cards = document.querySelectorAll('.card')
-
-
-
-
-// for (let card of cards) {
-
-//   card.addEventListener("click", function(){
-//     const videoId = card.getAttribute("id")
-//     window.location.href = `/video?id=${videoId}`
-
-//   })
-// }
